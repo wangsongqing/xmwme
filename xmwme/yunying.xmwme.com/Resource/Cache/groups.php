92a455fe45b09fef4dd6c09665e9a5092acdd258
jimmy <>
2018-02-11 20:16:07 +0800

    菜单goods

--- xmwme/yunying.xmwme.com/Resource/Cache/groups.php
+++ xmwme/yunying.xmwme.com/Resource/Cache/groups.php
@@ -4,5 +4,6 @@ $groups = array (
   'log' => array ('convert' => 'log','permition' => array ('index' => array ('convert' => '管理员登陆记录'))),
   'banner' => array ('convert' => 'banner','permition' => array ('index' => array ('convert' => 'banner管理'))),
   'activity' => array ('convert' => 'activity','permition' => array ('index' => array ('convert' => '活动管理'))),
+  'goods' => array ('convert' => 'goods','permition' => array ('index' => array ('convert' => '商品管理'))),
 );
 ?>
