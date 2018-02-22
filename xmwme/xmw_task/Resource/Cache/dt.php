cbfeb3bbc1a40074847da195c75fa4017c480c0a
jimmy <>
2018-02-22 17:08:15 +0800

    cmd 执行 计划任务

---
+++ xmwme/xmw_task/Resource/Cache/dt.php
@@ -0,0 +1,9 @@
+<?php
+$object['id'] = "dt";
+$object['enable'] = "1";
+$object['source'] = "../RunFramework/Util/TableManager.php";
+$object['className'] = "TableManager";
+$object['initMethod'] = "load";
+$object['property']['configFile'] = "Config/datatable.config.php";
+
+?>
