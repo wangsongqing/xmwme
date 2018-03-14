239e48bbe598cfc12e668dcbf99697a5eed1129c
jimmy <>
2018-03-14 16:31:54 +0800

    短信验证码

--- xmwme/wap.xmwme.com/App/Util/actionMiddleware.php
+++ xmwme/wap.xmwme.com/App/Util/actionMiddleware.php
@@ -78,7 +78,7 @@ class actionMiddleware extends Action {
         //把不需要登录就可以访问的方法写到这里 不需要登录就可以访问的方法key：model,value:actions
         $this->nologin = array(
             'index' => array('index','my','about','tell'),
-            'login' => array('index','password','regeze','ajaxlogin','verify','sendmsg','ajaxregister'),
+            'login' => array('index','password','regeze','ajaxlogin','verify','sendmsg','ajaxregister','forget'),
             'game'  => array('index'),
             'shop'  => array('index'),
             'goods' => array('index','detail'),
