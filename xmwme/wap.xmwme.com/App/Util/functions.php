e48012760707017cd9299a803a2801f0b44d547a
jimmy <>
2018-04-13 10:11:35 +0800

    mvc优化。。。。。

--- xmwme/wap.xmwme.com/App/Util/functions.php
+++ xmwme/wap.xmwme.com/App/Util/functions.php
@@ -1,7 +1,7 @@
 <?php
 //函数列表
-require_once('include/function.cookie.php');
-require_once('include/function.app.php');
-require_once('include/function.sesame.php');
-require_once('include/function.redis.php');
+require_once('../api/include/function.cookie.php');
+require_once('../api/include/function.global.php');
+require_once('../api/include/function.cache.php');
+require_once('include/function.auth.php');
 ?>
