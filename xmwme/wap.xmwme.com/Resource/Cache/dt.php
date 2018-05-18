effd2b95002697b4217de8e25ee3308fc7fe4319
jimmy <1105235512@qq.com>
2018-05-18 16:44:50 +0800

    配置优化

--- xmwme/wap.xmwme.com/Resource/Cache/dt.php
+++ xmwme/wap.xmwme.com/Resource/Cache/dt.php
@@ -4,6 +4,6 @@ $object['enable'] = "1";
 $object['source'] = "../RunFramework/Util/TableManager.php";
 $object['className'] = "TableManager";
 $object['initMethod'] = "load";
-$object['property']['configFile'] = "Config/datatable.config.php";
+$object['property']['configFile'] = "../Config/datatable.config.php";
 
 ?>
