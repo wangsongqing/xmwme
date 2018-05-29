af4cf93dce82e72b86e1372c9ef9a1ec0f0d3305
jimmy <1105235512@qq.com>
2018-05-29 10:46:22 +0800

    seo优化

--- xmwme/blog.xmwme.com/App/View/public/header.php
+++ xmwme/blog.xmwme.com/App/View/public/header.php
@@ -3,14 +3,14 @@
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
         <meta http-equiv="Cache-Control" content="no-cache">
-        <title>松松的博客 | 技术&amp;思想&amp;生活</title>
+        <title><?=$title?></title>
         <meta name="format-detection" content="telephone=no">
         <meta name="apple-mobile-app-status-bar-style" content="black">
         <meta name="apple-touch-fullscreen" content="YES">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="viewport" content="width=device-width, initial-scale=1.0,  minimum-scale=1.0, maximum-scale=1.0">
-        <meta name="keywords" content="">
-        <meta name="description" content="">
+        <meta name="keywords" content="<?=$title?>">
+        <meta name="description" content="<?=$scontent?>">
         <link href="<?= Resource ?>css/tip-twitter.css" rel="stylesheet">
         <link href="<?= Resource ?>css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
         <link rel="stylesheet" href="<?= Resource ?>css/tomorrow.css">
