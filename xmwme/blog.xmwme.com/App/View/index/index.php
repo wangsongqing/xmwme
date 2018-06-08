e2aca652f6c1a80348ae69317a6eb022fbfb4f4d
jimmy <1105235512@qq.com>
2018-06-08 13:59:31 +0800

    滚动加载数据！！

--- xmwme/blog.xmwme.com/App/View/index/index.php
+++ xmwme/blog.xmwme.com/App/View/index/index.php
@@ -4,8 +4,8 @@ $scontent = 'PHP|Mysql|Linux|Nginx|Memcache|Redis';
 ?>
 <?php include View.'/public/header.php'; ?>
         <!-- main container -->
-        <div class="container" style="margin-left: 340px;">
-            <?php foreach($data as $value){ ?>
+        <div class="container" style="margin-left: 340px;" id="main_gift_list">
+            <?php foreach($data['record'] as $value){ ?>
             <div class="article-list">
                 <article class="article">
                     <h1>
@@ -44,3 +44,39 @@ $scontent = 'PHP|Mysql|Linux|Nginx|Memcache|Redis';
         <div class="clear"></div>
     </div>
     <?php include View.'/public/footer.php'; ?>
+
+<script>  
+    var Page = 2;
+    var range = 10; //距下边界长度/单位px
+    var totalheight = 0;
+    var canRequest = true;
+    main =  $("#main_gift_list"); //主体元素
+    var str = '<div class="loading-more"><i></i>正在加载…</div>';
+    $(window).scroll(function(event){//滚动加载数据  
+           var wScrollY = window.scrollY; // 当前滚动条位置    
+           var wInnerH = window.innerHeight; // 设备窗口的高度（不会变）    
+           var bScrollH = document.body.scrollHeight; // 滚动条总高度        
+           if (wScrollY + wInnerH >= bScrollH) {  
+                $('.loading-more').remove();
+                main.append(str);
+                canRequest = false;
+                 $.ajax({
+                    type: "get",
+                    url : "/index/index/",
+                    async:false,
+                    dataType:'json',
+                    data: 'ajax=1&page=' + Page,
+                    success: function(result){
+                      $('.loading-more').remove();
+                        canRequest = true;
+                        if(result.err == '1' && result.data!="") {//查询结果有数据
+                          main.append(result.data);
+                          Page++;
+                        }else if(result.err == '0' && result.data ==""){//查询结果无数据
+                            canRequest = false;
+                        }
+                    }
+                });
+            }
+    });  
+</script>
