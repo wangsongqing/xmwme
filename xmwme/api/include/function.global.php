784e7b76b9ba04553ef08fca226323d07e760ec8
1105235512@qq.com <>
2018-08-31 14:43:38 +0800

    分类优化

--- xmwme/api/include/function.global.php
+++ xmwme/api/include/function.global.php
@@ -1073,68 +1073,19 @@ function format_money($money, $scale=2)
 }
 
 /**
- * 发送验证码短信
- * @param type $telephone
- * @param type $code
- * @param type $TemplateCode
+ * 调试打印方法
+ * @param type $param
  */
-function SmsSendText($telephone, $code, $TemplateCode){
-    require_once App."/Util/SignatureHelper.php";
-    $params = array ();
-
-    // *** 需用户填写部分 ***
-
-    // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
-    $accessKeyId = "LTAIQRhggaErpet3";
-    $accessKeySecret = "hXC6l277RbquTgihoQfTgvOSsk5ABd";
-
-    // fixme 必填: 短信接收号码
-    $params["PhoneNumbers"] = $telephone;
-
-    // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
-    $params["SignName"] = "食物百科网";
-
-    // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
-    $params["TemplateCode"] = $TemplateCode;
-
-    // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
-    $params['TemplateParam'] = Array (
-        "code" => $code,
-    );
-    // fixme 可选: 设置发送短信流水号
-    //$params['OutId'] = "12345";
-
-    // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
-    //$params['SmsUpExtendCode'] = "1234567";
-
-
-    // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
-    if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
-        $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+function dump($param=''){
+    if(is_array($param)){
+        echo '<pre>';
+        print_r($param);
+        exit;
+    }else{
+        var_dump($param);
+        exit;
     }
-
-    // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
-    $helper = new SignatureHelper();
-
-    // 此处可能会抛出异常，注意catch
-    try{
-        $content = $helper->request(
-            $accessKeyId,
-            $accessKeySecret,
-            "dysmsapi.aliyuncs.com",
-            array_merge($params, array(
-                "RegionId" => "cn-hangzhou",
-                "Action" => "SendSms",
-                "Version" => "2017-05-25",
-            ))
-        );
-    } catch (Exception $e) {
-        writeLog('用户：'.$telephone.'发送验证码失败，失败原因：'.$e->getMessage(), 'sms.log');
-    }
-    if($content->Code!='OK'){
-        writeLog(var_export($content),'return_sms_error.txt');
-    }
-    return $content;
+    
 }
 
 
