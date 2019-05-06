09581db8379b28a5694556918dd7b00bf48954c9
松松 <1105235512@qq.com>
2019-05-06 13:26:54 +0800

    更新 index.php

--- wap.xmwme.com/index.php
+++ wap.xmwme.com/index.php
@@ -1,4 +1,5 @@
 <?php
+//wsq
 set_time_limit(0);                                     //设置程序运行超时间
 ob_start();                                            //打开磁盘缓冲
 require_once('Config/apppath.config.php');             //初始化应用程序路径  
